6a6c3d6ef9b19c40e50ad5c8d7bb43e90f649e9b
Eshaan Kunchur <64331800+EshaanK8@users.noreply.github.com>
2022-07-14 22:17:07 -0400

    e

--- pages/exercises/[slug].js
+++ pages/exercises/[slug].js
@@ -208,15 +208,14 @@ export default function Post({exercise}) {
           <div className={styles.eTitleBox}>
             <h1 className={styles.eTitle}>About The {exercise.title}</h1>
           </div>
-          {(cart != null)
-          ? <div className={styles.eButtonBox}>
+          <div className={styles.eButtonBox}>
               {!(cart.filter(function(e) { return e.title === exercise.title; }).length > 0) ? (
               <Button className={styles.saveButton} onClick={() => addToCart({title: exercise.title, slug: exercise.slug})}>Save Exercise</Button>
               ) : (
                 <Button className={styles.savedButton}>Saved</Button>
               )}
             </div>
-          : <div></div>}
+          
         </div>
         <p className={styles.content}>{exercise.content}</p>
         <h1 className={styles.dTitle}>Discussion</h1>
